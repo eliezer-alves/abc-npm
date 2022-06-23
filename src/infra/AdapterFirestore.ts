437dca4169a7bc886da1091c25f621fb1af1c68c
eliezer <eliezer.c.alves2015@gmail.com>
2022-06-23 03:47:59 -0300

    feat: criando adapter da libe do firestore

--- src/infra/AdapterFirestore.ts
+++ src/infra/AdapterFirestore.ts
@@ -1,35 +1,11 @@
 import { db } from '@/config/firebase'
-import { addDoc, collection, doc, getDoc } from 'firebase/firestore'
+import { DBService } from '@/data/protocols'
+import { addDoc, collection } from 'firebase/firestore'
 
-export class AdapterFirestore {
-  constructor(
-    readonly ref: string,
-    readonly columns: Array<string>,
-    readonly primaryKey = 'id',
-  ) {}
-
-  async create(attr: any): Promise<string> {
-    const entity: any = {}
-    this.columns.map(column => {
-      return (entity[column] = attr[column] ?? null)
-    })
-    const response = await addDoc(collection(db, this.ref), entity)
+export class AdapterFirestore implements DBService {
+  async create(params: any): Promise<any> {
+    const response = await addDoc(collection(db, params.ref), params.body)
 
     return response.id
-  }
-
-  async find(id: string): Promise<any> {
-    let entity: any = {}
-    const docRef = doc(db, this.ref, id)
-    const docSnap = await getDoc(docRef)
-
-    if (docSnap.exists()) {
-      entity = docSnap.data()
-      entity.id = docSnap.id
-    } else {
-      return false
-    }
-
-    return entity
   }
 }
