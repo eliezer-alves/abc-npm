6a86574d6b3014314e2eb6e59131ff6841822ec4
eliezer <eliezer.c.alves2015@gmail.com>
2022-06-24 00:36:08 -0300

    feat (factories): creiando factory para instanciar um DBServer

---
+++ src/main/factories/infra/dbservice-factory.ts
@@ -0,0 +1,10 @@
+import { AdapterFirestore } from '@/infra/AdapterFirestore'
+
+export const makeDBService = (option = 'firestore') => {
+  switch (option) {
+    case 'firestore':
+      return new AdapterFirestore()
+    default:
+      return new AdapterFirestore()
+  }
+}
