301d880833c0dff2e19d6eeabe3dd107bde8abeb
eliezer <eliezer.c.alves2015@gmail.com>
2022-06-23 23:46:55 -0300

    feat: implementando metodo create na camada de infraestrutura

--- src/infra/AdapterFirestore.ts
+++ src/infra/AdapterFirestore.ts
@@ -1,11 +1,38 @@
 import { db } from '@/config/firebase'
-import { DBService } from '@/data/protocols'
+import { DBService, DBServiceCode, DBServiceResponse } from '@/data/protocols'
+import { UnauthorizedError } from '@/domain/errors'
 import { addDoc, collection } from 'firebase/firestore'
 
-export class AdapterFirestore implements DBService {
-  async create(params: any): Promise<any> {
-    const response = await addDoc(collection(db, params.ref), params.body)
+type ExpectedCreateResponse = {
+  id: string
+}
 
-    return response.id
+export class AdapterFirestore implements DBService {
+  public response: DBServiceResponse<any> = {
+    status: DBServiceCode.ok,
+  }
+
+  async create(params: any): Promise<DBServiceResponse<ExpectedCreateResponse>> {
+    let firestoreResponse
+    try {
+      firestoreResponse = await addDoc(collection(db, params.ref), params.body)
+    } catch (e: any) {
+      if (e.code === 'PERMISSION_DENIED') {
+        throw new UnauthorizedError()
+      }
+    }
+
+    if (firestoreResponse?.id) {
+      this.response.status = DBServiceCode.created
+      this.response.body = {
+        id: firestoreResponse.id,
+      }
+    } else {
+      this.response = {
+        status: DBServiceCode.badRequest,
+      }
+    }
+
+    return Promise.resolve(this.response)
   }
 }
