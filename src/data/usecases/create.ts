4a2d99f9f9df7722cab56534a20f3d764511b466
eliezer <eliezer.c.alves2015@gmail.com>
2022-06-24 02:01:41 -0300

    refactor: formatacao do codigo

--- src/data/usecases/create.ts
+++ src/data/usecases/create.ts
@@ -5,10 +5,7 @@ import { DBService, DBServiceCode } from '../protocols'
 export class Create {
   id?: string
 
-  constructor(
-    private readonly ref: string,
-    private readonly dbService: DBService<CreateResult>,
-  ) {}
+  constructor(private readonly ref: string, private readonly dbService: DBService<CreateResult>) {}
 
   async exec(params: CreateParams): Promise<CreateResult> {
     const result = await this.dbService.create({
