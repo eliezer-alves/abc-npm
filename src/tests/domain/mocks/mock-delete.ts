f196a6616b6f8df098af9b78d66eeeec88033feb
eliezer <eliezer.c.alves2015@gmail.com>
2022-06-28 09:38:31 -0300

    refactor - test (usecases - Delete): removendo metodo mockDeleteResult - inutil

--- src/tests/domain/mocks/mock-delete.ts
+++ src/tests/domain/mocks/mock-delete.ts
@@ -4,9 +4,3 @@ import { faker } from '@faker-js/faker'
 export const mockDeleteParam = (): DeleteParam => {
   return faker.random.alphaNumeric(10)
 }
-
-export const mockDeleteResult = (id = mockDeleteParam()): DeleteResult => ({
-  id: id,
-  email: faker.internet.email(),
-  password: faker.internet.password(),
-})
