7da280edbd0c38bcef67315bee788ec96c4924a3
eliezer <eliezer.c.alves2015@gmail.com>
2022-06-28 08:48:53 -0300

    test (main - BaseModel): testando metodo find apos dar um create

--- src/tests/main/base-model.spec.ts
+++ src/tests/main/base-model.spec.ts
@@ -12,7 +12,14 @@ const makeSut = (): SutTypes => {
   return { sut }
 }
 
+class Entity {
+  public data = {}
+  public id = ''
+}
+
 describe('BaseModel', () => {
+  const entity = new Entity()
+
   it('Should return correct response when execute create method', async () => {
     const { sut } = makeSut()
     const params = {
@@ -20,6 +27,19 @@ describe('BaseModel', () => {
       email: faker.internet.email(),
     }
     const result = await sut.create(params)
+    entity.data = params
+    entity.id = result.id
+
     expect(typeof result.id).toBe('string')
+  })
+
+  it('Should return correct response when execute find method before create', async () => {
+    const { sut } = makeSut()
+    const result = await sut.find(entity.id)
+
+    expect(result).toEqual({
+      id: entity.id,
+      ...entity.data,
+    })
   })
 })
