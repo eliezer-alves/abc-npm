e0884a7cc08038e4d99f9fc358421a0da7646493
eliezer <eliezer.c.alves2015@gmail.com>
2022-06-28 09:43:07 -0300

    feat (usecases - Delete): implementando interface domain/Delete em usecase Delete

--- src/data/usecases/delete.ts
+++ src/data/usecases/delete.ts
@@ -2,7 +2,7 @@ import { DBService, DBServiceCode } from '../../data/protocols'
 import { UnauthorizedError, UnexpectedError } from '../../domain/errors'
 import { Delete as IDelete, DeleteResult } from '../../domain/usecases'
 
-export class Delete {
+export class Delete implements IDelete {
   constructor(private readonly ref: string, private readonly dbService: DBService<DeleteResult>) {}
 
   async exec(id: string): Promise<DeleteResult> {
