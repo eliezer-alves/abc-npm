c6cf6f4dc64a3566cdfb5a211a5b4fa83cf063e2
eliezer <eliezer.c.alves2015@gmail.com>
2022-07-11 21:17:29 -0300

    test (usecases - Update): criando todo list do usecase Update

---
+++ src/tests/data/usecases/update/update.spec.ts
@@ -0,0 +1,13 @@
+describe('Update', () => {
+  it.todo('Should call DBServer with correct reference and params')
+
+  it.todo('Should throw UnauthorizedError if DBService returns 401')
+
+  it.todo('Should throw UnexpectedError if DBService returns 400')
+
+  it.todo('Should throw UnexpectedError if DBService returns 500')
+
+  it.todo('Should return of Update is currect if DBService returns 201')
+
+  it.todo('Should throw UnexpectedError when return of Update is empty but DBService returns 201')
+})
